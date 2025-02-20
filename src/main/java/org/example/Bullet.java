ce7dc6c40ff4cf88d318a4ae1df6ca7dc59f96bf
Madumarov Davron <menaceteacher@gmail.com>
2025-02-20 10:52:10 +0500

    add

--- src/main/java/org/example/Bullet.java
+++ src/main/java/org/example/Bullet.java
@@ -1,11 +1,11 @@
 package org.example;
 
 public class Bullet {
-//    int x, y, speed = 10, angle;
-//
-//    Bullet(int x, int y, int angle) {
-//        this.x = x;
-//        this.y = y;
-//        this.angle = angle;
-//    }
+    int x, y, speed = 10, angle;
+
+    Bullet(int x, int y, int angle) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+    }
 }
