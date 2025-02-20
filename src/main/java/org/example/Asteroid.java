24d6df9a02c2e7b9c382fa47a2719336876ae6a7
Madumarov Davron <menaceteacher@gmail.com>
2025-02-20 10:52:39 +0500

    add

--- src/main/java/org/example/Asteroid.java
+++ src/main/java/org/example/Asteroid.java
@@ -2,12 +2,12 @@ package org.example;
 
 public class Asteroid {
 
-//    int x, y, dx, dy;
-//
-//    Asteroid(int x, int y, int dx, int dy) {
-//        this.x = x;
-//        this.y = y;
-//        this.dx = dx;
-//        this.dy = dy;
-//    }
+    int x, y, dx, dy;
+
+    Asteroid(int x, int y, int dx, int dy) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+    }
 }
